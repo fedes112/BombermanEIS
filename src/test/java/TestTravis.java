b34f817be9cfa521bd2d70be1522a6167e930bfb
fedes112 <fedes112@users.noreply.github.com>
2018-10-10 00:38:23 -0300

    fst commit

---
+++ src/test/java/TestTravis.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestTravis {
+
+    @Test
+       public void testeoPrueba() {
+        assertTrue(true);
+
+    }
+
+}
